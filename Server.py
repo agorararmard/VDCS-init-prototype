2fa10c61ec41f997686c1e3f206376c28ddcda43
AmrGouhar <agouhar@synapse-analytics.io>
2019-10-05 13:35:17 +0200

    init

--- Server.py
+++ Server.py
@@ -1,2 +1,19 @@
+class Garbled_Circuit:
+    def garble(self):
+        return self
+
 class Server:
     
+    IDs = 0
+
+    def __init__(self):
+        self.ID = (Server.IDs+1)
+        Server.IDs += 1
+
+
+    def getID(self):
+        return self.ID
+
+    
+
+
